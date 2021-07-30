3d15d25fbd78e09909786396b8fdd3478d4c0f2b
Ketrin <geleczkak26@gmail.com>
2021-07-30 16:19:57 +0300

    Add file

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// let $number = document.querySelector('#number');
+// let $decrease = document.querySelector('#decrease');
+// let $reset = document.querySelector('#reset');
+// let $increase = document.querySelector('#increase');
+
+// let text = $number.textContent;
+// let number = Number(text);
+
+// $number.style.color = 'white';
+
+// $decrease.addEventListener('click', decreaseFunction);
+// $reset.addEventListener('click', resetFunction);
+// $increase.addEventListener('click', increaseFunction);
+
+// function decreaseFunction() {
+//     number--;
+//     $number.innerHTML = Number(number);
+//     setColor();
+// }
+
+// function resetFunction() {
+//     number = 0;
+//     $number.innerHTML = Number(number);
+//     setColor();
+// }
+
+// function increaseFunction() {
+//     number++;
+//     $number.innerHTML = Number(number);
+//     setColor();
+// }
+
+// console.log(typeof($number));
+// console.log($number);
+
+// function setColor() {
+//     if (number < 0) {
+//         $number.style.color = 'red';
+//     } else if (number > 0) {
+//         $number.style.color = 'green';
+//     } else if (number === 0){
+//         $number.style.color = 'white';
+//     }
+// }
